0d03b84bb169fe9ed623c96a7c84d51cf07ba089
Vladimir Kotikov <v-vlkoti@microsoft.com>
2014-07-03 12:46:31 +0400

    Fixed platform add step failure due to CB-6776 Make project/.cordova/config.json integrate with platforms.js

--- writejson.js
+++ writejson.js
@@ -1,3 +1,5 @@
+/*jshint multistr: true */
+
 var path = require ('path');
 var shell = require('shelljs');
 var fs = require('fs');
@@ -6,7 +8,7 @@ var argv = require('optimist').argv;
 var TEST_DIR=process.cwd();
 var MSPEC_DIR=path.join(TEST_DIR,'mobilespec');
 var BRANCH="dev";
-// required on Windows to correcctly escape path delimiter character
+// required on Windows to correctly escape path delimiter character
 var TEST_DIR_ESCAPED = TEST_DIR.split("\\").join("\\\\");
 
 if(argv.branch) BRANCH=argv.branch;
@@ -22,31 +24,37 @@ fs.writeFileSync(path.join(MSPEC_DIR,'.cordova','config.json'),
   "lib": {\
     "android": {\
       "uri": "'+TEST_DIR_ESCAPED+'/cordova-android",\
+      "url": "'+TEST_DIR_ESCAPED+'/cordova-android",\
       "version": "'+BRANCH+'" ,\
       "id": "cordova-android-'+BRANCH+'"\
     },\
     "ios": {\
       "uri": "'+TEST_DIR_ESCAPED+'/cordova-ios",\
+      "url": "'+TEST_DIR_ESCAPED+'/cordova-ios",\
       "version": "'+BRANCH+'",\
       "id": "cordova-ios-'+BRANCH+'"\
     },\
     "wp8": {\
       "uri": "'+TEST_DIR_ESCAPED+'/cordova-wp8",\
+      "url": "'+TEST_DIR_ESCAPED+'/cordova-wp8",\
       "version": "'+BRANCH+'",\
       "id": "cordova-wp8-'+BRANCH+'"\
     },\
     "windows8": {\
       "uri": "'+TEST_DIR_ESCAPED+'/cordova-windows",\
+      "url": "'+TEST_DIR_ESCAPED+'/cordova-windows",\
       "version": "'+BRANCH+'",\
       "id": "cordova-windows-'+BRANCH+'"\
     },\
     "windows": {\
       "uri": "'+TEST_DIR_ESCAPED+'/cordova-windows",\
+      "url": "'+TEST_DIR_ESCAPED+'/cordova-windows",\
       "version": "'+BRANCH+'",\
       "id": "cordova-windows-'+BRANCH+'"\
     },\
     "blackberry": {\
       "uri": "'+TEST_DIR_ESCAPED+'/cordova-blackberry",\
+      "url": "'+TEST_DIR_ESCAPED+'/cordova-blackberry",\
       "version": "'+BRANCH+'",\
       "id": "cordova-blackberry-'+BRANCH+'"\
     }\
